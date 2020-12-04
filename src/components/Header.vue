27a089e7778841b133215166e153cdb38e939832
Long Vu <redralogro@gmail.com>
2020-12-04 17:25:22 +0700

    add logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,11 @@
 <template>
   <div class="hello">
   <div class="row">
-  <div class="col-md-3" style="background-color:red;height:10px;"></div>
-  <div class="col-md-3" style="background-color:blue;height:10px;"></div>
-  <div class="col-md-3" style="background-color:green;height:10px;"></div>
-  <div class="col-md-3" style="background-color:black;height:10px;"></div>
-  </div>
-        <router-link to="/">Home</router-link>
+  <div class="image"><img src="static/img/logo.png" alt=""></div>
+      <router-link to="/">Home</router-link>
        <router-link to="/wallet">Connect to Wallet</router-link>
        <router-link to="/about-us">About Us</router-link>
+  </div>
 </div>
 </template>
 
@@ -24,6 +21,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+img {
+  margin-left: 10px;
+  width:180px;
+  float:left;
+}
 li {
   margin-left: 10px;
   margin-right: 10px;
