cbc7b2a32d9ce96a1737630af6d567c84d9154e4
Long Vu <redralogro@gmail.com>
2020-12-10 10:00:35 +0700

    change LineChart position

--- src/components/Charts/LineChart.vue
+++ src/components/Charts/LineChart.vue
@@ -38,19 +38,19 @@ export default {
         ],
         datasets: [
           {
-            label: "Data One",
-            borderColor: "#FC2525",
-            pointBackgroundColor: "white",
+            label: 'Data One',
+            borderColor: '#FC2525',
+            pointBackgroundColor: 'white',
             borderWidth: 1,
-            pointBorderColor: "white",
+            pointBorderColor: 'white',
             backgroundColor: this.gradient,
             data: [40, 39, 10, 40, 39, 80, 40]
           },
           {
-            label: "Data Two",
-            borderColor: "#05CBE1",
-            pointBackgroundColor: "white",
-            pointBorderColor: "white",
+            label: 'Data Two',
+            borderColor: '#05CBE1',
+            pointBackgroundColor: 'white',
+            pointBorderColor: 'white',
             borderWidth: 1,
             backgroundColor: this.gradient2,
             data: [60, 55, 32, 10, 2, 12, 53]
