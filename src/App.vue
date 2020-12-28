f39312b4771917e9a3c94db526f56c369e25aa8d
Long Vu <redralogro@gmail.com>
2020-12-28 14:01:22 +0700

    Add form login

--- src/App.vue
+++ src/App.vue
@@ -29,12 +29,11 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 .View {
   margin-top:40px;
-  min-height:921px;
+  min-height:900px;
   background-color:#FAFAFA;
 }
 </style>
--- src/components/users/Login.vue
+++ src/components/users/Login.vue
@@ -1,9 +1,27 @@
 <template>
   <div class="Login">
-    <div class ="row title" style="">
-      <h1 style ="float:left">Login</h1>
+    <h1 style ="float:left;width:100%;">Login</h1>
+    <div class="row card-header">
+    <span style ="text-align:center;color:#FFF;margin: 0 auto;font-size: 36px;">Login with email</span>
+    </div> 
+    <div class=" login-email-box mx-auto">
+    <form>
+      <div class="form-group">
+        <span for="exampleInputEmail1">Email address</span>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div class="form-group">
+        <span for="exampleInputPassword1">Password</span>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+      </div>
+      <button type="submit" class="btn btn-danger">Submit</button>
+      <div class="form-group">
+        <a><span>Forgot Password ?</span></a>
+        <router-link to="/myaccount/register" style="float:right;">Sign Up</router-link>
+      </div>
+    </form>
     </div>
-    <div class="row card-header"></div> 
   </div>      
 </template>
 
@@ -22,6 +40,14 @@ export default {
 h1, h2 {
   font-weight: normal;
 }
+button {
+  width: 100%;
+  height: 4.375rem;
+  margin-left: 0px; 
+}
+input {
+ height: 4.375rem
+}
 .title  > h1 {
   padding-left: 11px;
 }
@@ -30,6 +56,10 @@ h1, h2 {
   background-color:#474747;
   width:100%;
   margin-left:0px;
+}
+.login-email-box{
+  padding-top:2.5rem;
+   width:50%;
 }
 ul {
   list-style-type: none;
@@ -40,6 +70,6 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: #6C757D;
 }
 </style>
